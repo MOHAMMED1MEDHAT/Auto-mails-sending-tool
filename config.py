6ade44756e6b2e356a4e06355d200c14aeef758f
mohammed medhat <77582889+MOHAMMED1MEDHAT@users.noreply.github.com>
2022-11-03 17:37:32 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SENDER_EMAIL_ADDRESS = "demo22793@gmail.com"
-SENDER_PASSWORD = "0123456./"
+SENDER_EMAIL_ADDRESS = "demo22793@gmail.com"#sender email
+SENDER_PASSWORD = "0123456./"#sender password
 RECEIVER_EMAIL_ADDRESS=["01028605670m@gmail.com"] #list of the emails
